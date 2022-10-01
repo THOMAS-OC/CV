904dbee6a99812c48ea38e73935fb61636691904
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-01 14:46:47 +0200

    Section contact ok

--- app.js
+++ app.js
@@ -46,3 +46,19 @@ typeWriter
 .start()
 
 // END Animation accueil
+
+// Animation contact
+
+const inputFields = document.querySelectorAll("input")
+
+for (let i = 0; i < inputFields.length; i++) {
+    let field = inputFields[i];
+    field.addEventListener("input", (e) => {
+        if (e.target.value !== "") {
+            e.target.parentNode.classList.add("animation")
+        }
+        else if (e.target.value == "") {
+            e.target.parentNode.classList.remove('animation')
+        }
+    })
+}
